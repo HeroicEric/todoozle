55a5e1704cbefd57f65221f209026b0a76ea23ae
Eric Kelly <heroiceric@gmail.com>
2013-01-31 20:06:30 -0500

    remove unnecessary validates on tasks model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,4 @@ class Task < ActiveRecord::Base
   attr_accessible :title, :complete
 
   validates_presence_of :title
-  validates_presence_of :complete
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,7 +2,6 @@ require "spec_helper"
 
 describe Task do
   it { should validate_presence_of :title }
-  it { should validate_presence_of :complete }
 
   describe "default value for complete" do
     it "returns false" do
