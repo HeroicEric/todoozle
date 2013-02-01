9f4461c362f4c413bfd2109c93592a5562fea71c
Eric Kelly <heroiceric@gmail.com>
2013-01-31 21:44:06 -0500

    move all routes to one file for now

--- app/assets/javascripts/routes/app_router.js
+++ app/assets/javascripts/routes/app_router.js
@@ -1,3 +1,9 @@
 App.Router.map(function() {
   this.resource('tasks');
 });
+
+App.TasksRoute = Ember.Route.extend({
+  model: function() {
+    return App.Task.find();
+  }
+});
--- app/assets/javascripts/routes/tasks_route.js
+++
@@ -1,5 +0,0 @@
-App.TasksRoute = Ember.Route.extend({
-  model: function() {
-    return App.Task.find();
-  }
-});
